5c378a7c5687844c5e2b1193fa51c2bfeb5e20d2
eduardoVillar04 <132345105+eduardoVillar04@users.noreply.github.com>
2026-05-10 11:44:36 +0200

    Update ESLint configuration rules and styles

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,41 @@
 // eslint.config.js
 module.exports = [
-    {
-        "rules": {
-            "no-cond-assign": [
-                "error",
-                "always"
-            ],
-            "indent": [
-                "error",
-                2
-            ],
-            "linebreak-style": [
-                "error",
-                "unix"
-            ],
-            "quotes": [
-                "error",
-                "single"
-            ],
-            "no-unused-vars": "off",
-            "semi": [
-                "error",
-                "always"
-            ]
-        }
-    }
+	{
+		rules: {
+			'no-cond-assign': [
+				'error',
+				'always'
+			],
+
+			indent: [
+				'error',
+				'tab',
+				{
+					SwitchCase: 1
+				}
+			],
+
+			'linebreak-style': [
+				'error',
+				'unix'
+			],
+
+			quotes: [
+				'error',
+				'double'
+			],
+
+			'no-unused-vars': 'warn',
+
+			semi: [
+				'error',
+				'always'
+			],
+
+			brace-style: [
+				'error',
+				'1tbs'
+			]
+		}
+	}
 ];
